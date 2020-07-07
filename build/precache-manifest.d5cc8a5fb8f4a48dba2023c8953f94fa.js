32e25b77ed60002fd068ab598a7c84288fd2a04c
xomrau <hle3524@gmail.com>
2020-07-07 11:48:19 +1000

    added build folder

---
+++ build/precache-manifest.d5cc8a5fb8f4a48dba2023c8953f94fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22578ce9fce3aae4bdf188efc846c49",
+    "url": "/react-app-host-testing/index.html"
+  },
+  {
+    "revision": "6cea34fc4bd425902ebc",
+    "url": "/react-app-host-testing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9ff9bdb4f2f0a211ef28",
+    "url": "/react-app-host-testing/static/js/2.e11b663d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-host-testing/static/js/2.e11b663d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cea34fc4bd425902ebc",
+    "url": "/react-app-host-testing/static/js/main.62e87eea.chunk.js"
+  },
+  {
+    "revision": "5f66992e35176d6ce6d5",
+    "url": "/react-app-host-testing/static/js/runtime-main.499ee3a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-host-testing/static/media/logo.5d5d9eef.svg"
+  }
+]);
